08bdd350c9b0305ba407b9f8f03cf39441e6455b
Andrew Li <andrewjli@utexas.edu>
2024-09-27 14:15:29 -0500

    Add digits dp solution

--- DP/digits.cpp
+++ DP/digits.cpp
@@ -14,9 +14,35 @@ struct pii {
 
 using namespace std;
 
+
+vector<int> digitize(int n, vector<int> rslt) {
+    while (n != 0) {
+        rslt.pb(n % 10);
+        n /= 10;
+    }
+    return rslt;
+}
+
+
+
 int main() {
     int n;
     cin >> n;
 
-    
+    vector<int> dp(n + 1, 1e9);
+    dp[n] = 0;
+    for (int i = n; i > 0; i--) {
+        if (dp[i] == 1e9) {
+            continue;
+        }
+        vector<int> digits;
+        digits = digitize(i, digits);
+        for (int j = 0; j < sz(digits); j++) {
+            if (i - digits[j] < 0) {
+                continue;
+            }
+            dp[i - digits[j]] = min(dp[i - digits[j]], dp[i] + 1);
+        }
+    }
+    cout << dp[0] << '\n';
 }

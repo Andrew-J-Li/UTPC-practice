e9e817fe392dd9c1c290aa4eda802993b9ed2e0c
Andrew Li <andrewjli@utexas.edu>
2024-12-01 23:07:56 -0600

    Add Mortal Kombat Problem

---
+++ DP/mortalKombat.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        int N;
+        cin >> N;
+        vector<int> bosses(N);
+        for (int j = 0; j < N; j++) {
+            cin >> bosses[j];
+        }
+
+        vector<int> dp(N + 1, 0);
+        dp[0] = 0;
+        for (int j = 0; j <= N; j++) {
+            
+        }
+    }
+}

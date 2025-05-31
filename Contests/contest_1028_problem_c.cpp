4a6f0406caf3576c1c89856be9d3eb8534f3978a
Andrew Li <andrewjli@utexas.edu>
2025-05-31 11:05:32 -0500

    finish contest_1028_problem_c

--- Contests/contest_1028_problem_c.cpp
+++ Contests/contest_1028_problem_c.cpp
@@ -21,13 +21,41 @@ int main() {
     while (t--) {
         ll n; cin >> n;
         vector<ll> nums (n);
-        ll target = -1;
+        vector<ll> idx;
+        ll target = -1, mn = LLONG_MAX;
         for (ll i = 0; i < n; ++i) {
             cin >> nums[i];
-            if (target == -1) target = nums[i];
-            else target = gcd(target, nums[i]);
+            mn = min(mn, nums[i]);
+            if (target == -1) {
+                target = nums[i];
+                idx.push_back(i);
+                continue;
+            }
+            ll prev = target;
+            target = gcd(target, nums[i]);
+            if (prev != target) idx.push_back(i);
         }
-        
+        ll rslt = 0;
+        if (mn == target) {
+            for (ll i = 0; i < n; ++i) if (nums[i] != mn) rslt++;
+            cout << rslt << endl;
+            continue;
+        }
+        mn = LLONG_MAX;
+        for (ll i = 0; i < n; ++i) {
+            ll curr = nums[i];
+            ll temp = 0;
+            for (ll j = idx.size() - 1; j >= 0; --j) {
+                ll prev = curr;
+                curr = gcd(curr, nums[idx[j]]);
+                if (prev != curr) temp++;
+                if (curr == target) break;
+            }
+            mn = min(mn, temp);
+        }
+        rslt += mn;
+        rslt += n - 1;
+        cout << rslt << endl;
     }
     return 0;
 }

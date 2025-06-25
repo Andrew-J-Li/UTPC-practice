9db2eb828f9f99708f163b4f727141e2fc79e63f
Andrew-J-Li <andrewjli@utexas.edu>
2025-06-24 19:35:39 -0500

    finish round_1027_problem_e

--- Contests/round_1027_problem_e.cpp
+++ Contests/round_1027_problem_e.cpp
@@ -11,24 +11,36 @@ int main() {
     ll t; cin >> t;
     while (t--) {
         ll n; cin >> n;
+        vector<ll> weights (n);
+        for (ll i = 0; i < n; ++i) cin >> weights[i];
+        vector<pair<ll, ll>> dp (n, {LLONG_MIN, 0});
         vector<vector<ll>> adj (n);
-        vector<ll> w (n);
-        for (ll i = 0; i < n; ++i) cin >> w[i];
-        for (ll i = 0; i < n; ++i) {
-            ll a, b; cin >> a >> b; a--; b--;
+        for (ll i = 0; i < n - 1; ++i) {
+            ll a, b; cin >> a >> b;
+            a--; b--;
             adj[a].push_back(b);
             adj[b].push_back(a);
         }
-        queue<pair<ll, pair<ll, ll>>> q;
-        vector<ll> rslt (n);
-        rslt[0] = w[0];
-        for (ll i = 0; i < adj[0].size(); ++i) q.push({1, {w[0], 0}});
+        vector<bool> v (n);
+        dp[0].first = weights[0];
+        dp[0].second = 0;
+        v[0] = true;
+        queue<pair<ll, ll>> q;
+        for (ll i = 0; i < adj[0].size(); ++i) q.push({adj[0][i], 0});
         while (!q.empty()) {
-            pair<ll, pair<ll, ll>> curr = q.front();
+            pair<ll, ll> curr = q.front();
             q.pop();
-            ll dist = curr.first, odd = curr.second.first, even = curr.second.second;
-            
+            if (v[curr.first]) continue;
+            ll idx = curr.first, parent = curr.second;
+            v[idx] = true;
+
+            ll weight = weights[idx];
+            dp[idx].first = max(dp[idx].first, weight - dp[parent].second);
+            dp[idx].second = min(dp[idx].second, weight - dp[parent].first);
+            for (ll i = 0; i < adj[idx].size(); ++i) q.push({adj[idx][i], idx});
         }
+        for (pair<ll, ll> it : dp) cout << it.first << " ";
+        cout << endl;
     }
     return 0;
 }

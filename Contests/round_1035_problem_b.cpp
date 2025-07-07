0f2d1d4d0c3d23c6dee968a5ff6770941ee9fc73
Andrew-J-Li <andrewjli@utexas.edu>
2025-07-06 19:21:26 -0500

    upsolve round 1035 problem b

--- Contests/round_1035_problem_b.cpp
+++ Contests/round_1035_problem_b.cpp
@@ -12,12 +12,28 @@ int main() {
     ll t; cin >> t;
     while (t--) {
         ll n; cin >> n;
-        pair<ll, ll> p, q;
+        pair<double, double> p, q;
         cin >> p.first >> p.second >> q.first >> q.second;
-        vector<ll> ops (n);
-        for (ll i = 0; i < n; ++i) cin >> ops[i];
+        vector<double> ops (n);
+        double sum = 0;
+        for (ll i = 0; i < n; ++i) {
+            cin >> ops[i];
+            sum += ops[i];
+        }
+        double dist = (q.first - p.first) * (q.first - p.first) + (q.second - p.second) * (q.second - p.second);
+        dist = sqrt (dist);
+        if (n == 1) {
+            cout << ((sum == dist) ? "YES" : "NO") << endl;
+            continue;
+        }
+
+        sum += dist;
+        ops.push_back(dist);
+
         sort (ops.begin(), ops.end(), greater<ll>());
-        cout << ops[0] << endl;
+        double mx = ops[0]; sum -= ops[0];
+        if (mx <= sum) cout << "YES" << endl;
+        else cout << "NO" << endl;
     }
     return 0;
 }
